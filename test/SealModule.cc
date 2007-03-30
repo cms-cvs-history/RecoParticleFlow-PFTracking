8dc750559cb865d129ebd4e012988c811722530e
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2007-03-30 13:14:05 +0000

    first commit

---
+++ test/SealModule.cc
@@ -0,0 +1,8 @@
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/InputSourceMacros.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "RecoParticleFlow/PFTracking/test/TestPreId.h"
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(TestPreId);
+
+
