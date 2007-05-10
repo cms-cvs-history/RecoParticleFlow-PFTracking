360e9b83022a2b81b8812e70beefdc1da1f42ff7
cmsbuild <>
2007-05-10 23:03:30 +0000

    This commit was manufactured by cvs2git to create tag 'RecoParticleFlow- PFTracking-test4'.
    Sprout from RecoParticleFlow-PFTracking-compat_1_5_0 2007-05-10 23:03:28 UTC Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch> 'merged from HEAD, before_trunk2branch_2. conflicts solved. untested because of problems in other packages in this nightly'
    Delete:
        BuildFile
        data/Threshold.dat
        data/elecPreId.cff
        data/elecPreId.cfi
        data/elecPreId_refit.cff
        data/particleFlowTrack.cff
        data/particleFlowTrack_refit.cff
        data/pfTrackElec.cfi
        doc/html/index.html
        doc/html/overview.html
        interface/PFElecTkProducer.h
        interface/PFTrackTransformer.h
        src/GoodSeedProducer.cc
        src/PFElecTkProducer.cc
        src/PFTrackTransformer.cc
        src/SealModule.cc
        test/BuildFile
        test/SealModule.cc
        test/TestPreId.cc
        test/TestPreId.cff
        test/TestPreId.cfi
        test/TestPreId.h
        test/test.cfg

--- src/SealModule.cc
+++
@@ -1,8 +0,0 @@
-#include "FWCore/Framework/interface/InputSourceMacros.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoParticleFlow/PFTracking/interface/GoodSeedProducer.h"
-#include "RecoParticleFlow/PFTracking/interface/PFElecTkProducer.h"
-
-DEFINE_ANOTHER_FWK_MODULE(GoodSeedProducer);
-DEFINE_ANOTHER_FWK_MODULE(PFElecTkProducer);
-
--- test/SealModule.cc
+++
@@ -1,8 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/InputSourceMacros.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoParticleFlow/PFTracking/test/TestPreId.h"
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(TestPreId);
-
-
