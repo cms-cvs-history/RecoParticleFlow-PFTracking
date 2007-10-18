b6621020019e29c22d041217abae0030ab2874b3
cmsbuild <>
2007-10-18 13:18:25 +0000

    This commit was manufactured by cvs2git to create tag 'RecoParticleFlow- PFTracking-V02-05-02-01'.
    Sprout from master 2007-08-10 14:17:25 UTC Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch> 'bug fix'
    Cherrypick from master 2007-10-18 13:18:08 UTC Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch> 'adapted to changes in 180 cycle':
        test/FakeEval.cc
        test/SealModule.cc
    Delete:
        test/TestPreId.cc
        test/TestPreId.cff
        test/TestPreId.cfi
        test/TestPreId.h
        test/test.cfg

--- test/SealModule.cc
+++ test/SealModule.cc
@@ -1,9 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "RecoParticleFlow/PFTracking/test/TestPreId.h"
 #include "RecoParticleFlow/PFTracking/test/FakeEval.h"
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(TestPreId);
 DEFINE_ANOTHER_FWK_MODULE(FakeEval);
 
